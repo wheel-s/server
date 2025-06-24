966f1a08cf165ac7879dba9e15203b18993ebd2e
wheel-s <toshirou@gmail.com>
2025-06-24 13:29:12 +0100

    changed limit

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cors({origin:"*"}))
 app.use(helmet())
 
 
-app.use(express.json())
+app.use(express.json({limit:'1mb'}))
 
 express.json({limit:'10mb'})
 express.urlencoded({limit:'10mb'})
